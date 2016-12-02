be91ff349007100104691fb61021dfd6ce2d4881
Mirza Ohranovic <mirza.ohranovic@gmail.com>
2016-12-02 20:20:13 +0100

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+<!-- Cover image and call to action -->
+<div class="cover-image">
+  <div class="col-md-offset-2 col-md-8 col-sm-12">
+    <h1>Doniraj socijalno ugroženima</h1>
+    <span class="opis">Fabrika sreće je servis koji pruža ljudima mogućnost da brzo i efikasno pristupe procesu doniranja sredstava i pomaganja drugima.</span>
+      <div class="clearfix"></div>
+
+    <a class="btn btn-success col-md-offset-1 col-md-5 col-sm-12">Doniraj</a>
+    <a class="btn btn-default col-md-offset-1 col-md-5 col-sm-12">Budi aktivan</a>
+  </div>
+</div>
+<!-- Endof cover image and call to action -->
+
+<div class="container">
+    <div class="row">
+
+
+
+
+    </div>
+</div>
+@endsection
