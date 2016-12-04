553f8db7df998ca70e9c0dc00487691aca6e45a0
Mirza Ohranovic <mirza.ohranovic@gmail.com>
2016-12-04 03:40:15 +0100

    k

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <!-- Cover image and call to action -->
-<div class="cover-image">
+<div class="cover-image short-cover">
   <div class="col-md-offset-2 col-md-8 col-sm-12">
     <h1>Pomozi socijalno ugroženima</h1>
     <span class="opis">Fabrika sreće je <b>servis</b> koji pruža ljudima mogućnost da brzo i efikasno pristupe procesu doniranja sredstava i pomaganja drugima.</span>
@@ -10,8 +10,8 @@
 
       <hr style="border-color:#e7e7e7;width:20%;margin-top:25px;"/>
 
-    <a class="btn btn-success col-md-offset-4 col-md-4 col-sm-12 btnfix" href="{{url('/donations/create')}}">Doniraj</a>
-    <a class="btn btn-default col-md-offset-4 col-md-4 col-sm-12 btnfix" href="{{url('/subscribe')}}">Budi aktivan</a>
+    <a class="btn btn-success col-md-offset-4 col-md-4 col-sm-12 col-xs-12 btnfix" href="{{url('/donations/create')}}">Doniraj</a>
+    <a class="btn btn-default col-md-offset-4 col-md-4 col-sm-12 col-xs-12 btnfix" href="{{url('/subscribe')}}">Budi aktivan</a>
   </div>
 </div>
 <!-- Endof cover image and call to action -->
@@ -19,11 +19,11 @@
 <div class="part">
   <div class="container">
       <div class="row">
-        <div class="col-md-6 col-sm-12" style="text-align:right;">
+        <div class="al-c col-md-6 col-sm-12" style="text-align:right;">
           <h2>O Fabrici sreće</h2>
           Fabrika sreće je servis koji pruža uvid u registar donacija namijenjenih kao pomoć socijalno ugroženima osobama i grupama. Ono što karakteriše servis koji pruža Fabrika sreće jeste jednostavan i brz način unosa informacija o donaciji, kao i pregledan i efikasan pronalazak istih od strane humanitarnih organizacija. Naš prvenstveni cilj jeste da povežemo i olakšamo humanitarnim organizacijama proces pronalaska sredstava i resursa kako bi adekvatno pružile pomoć svim ljudima koji se nalaze u ugroženoj skupini.
         </div>
-        <div class="col-md-6 col-sm-12">
+        <div class="al-c col-md-6 col-sm-12">
           <h2>Supporter club</h2>
           Dio Supporter club-a Fabrike sreće možete postati na dva načina: direktnom donacijom finansijskih sredstava u cilju pomoći radu i funkcionisanju servisa ili donacijom određenih sredstava (finansijskih/materijalnih) u humanitarne svrhe putem našeg servisa. Članovi Supporter club-a će svoje ime imati objavljeno na našoj Hall of fame stranici i bit će istaknuti kao humanitarci i učesnici u procesu pomaganja socijalno ugroženima i obespravljenima.
         </div>
@@ -37,10 +37,10 @@
 <div class="part">
   <div class="container">
       <div class="row">
-        <div class="col-md-5 col-sm-12">
+        <div class="al-c col-md-5 col-sm-12">
           <img src="{{ url('img/mockup.png')}}" style="width:100%;" />
         </div>
-        <div class="col-md-7 col-sm-12" style="margin-bottom:30px;">
+        <div class="al-c col-md-7 col-sm-12" style="margin-bottom:30px;">
           <h2>FS Widget - Build the community!</h2>
           Jedan od načina da pomognete radu i promociji Fabrike sreće jeste da implementirate naš widget na svojoj web stranici/servisu te na taj način olakšate proces prijavljivanja donacije u naš registar. Također, implementacijom widgeta na svojoj web stranici pomažete u procesu promocije i jačanja mreže humanitaraca i humanitarnih organizacija na području Bosne i Hercegovine.
             <br /><br />
